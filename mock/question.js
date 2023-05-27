47896d95868d0aaf2a53bb096dd63219e3a17a7e
xiewen <1123654054@qq.com>
2023-05-27 10:31:16 +0800

    复制mock

--- mock/question.js
+++ mock/question.js
@@ -63,5 +63,18 @@ module.exports = [
            
             }
         }
-    }
+    },
+    {
+        //复制问卷
+        url:'/api/question/duplicate/:id',
+        method:'post',
+        response(){
+            return {
+                errno:0,
+                data:{
+                    id:Random.id()
+                }
+            }
+        }
+    },
 ]
